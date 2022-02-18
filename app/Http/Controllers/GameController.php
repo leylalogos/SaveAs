c7aa5efb58948bca3a59d3e3d1baa7adef5a9b54
leyla sabouri <dev.leyla.sabouri@gmail.com>
2022-02-18 22:06:20 +0330

    set redis cache

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -5,11 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Game;
 use App\Http\Resources\GameResource;
+use Illuminate\Support\Facades\Cache;
+
 
 class GameController extends Controller
 {
     public function show_games()
     {
-        return GameResource::collection(Game::all());
+        return Cache::remember('games', 5, function () {
+            return GameResource::collection(Game::all());
+        });
     }
 }
--- app/Http/Controllers/ScoreController.php
+++ app/Http/Controllers/ScoreController.php
@@ -6,13 +6,12 @@ use Illuminate\Http\Request;
 use App\Models\Play;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
 
 class ScoreController extends Controller
 {
     public function addScore(Request $request, $game_id, $user_id)
     {
-        return User::find($user_id)->user_rank;
-
         Play::create([
             'game_id' => $game_id,
             'user_id' => $user_id,
@@ -22,12 +21,14 @@ class ScoreController extends Controller
     }
     public function scoreBoard($game_id)
     {
-        return DB::table('plays')
-            ->select(DB::raw('user_id,sum(score) as total'))
-            ->where('game_id', $game_id)
-            ->groupBy('user_id')
-            ->orderBy('total', "desc")
-            ->limit(25)
-            ->get();
+        return Cache::remember('scoreBoard', 1, function () use ($game_id) {
+            return DB::table('plays')
+                ->select(DB::raw('user_id,sum(score) as total'))
+                ->where('game_id', $game_id)
+                ->groupBy('user_id')
+                ->orderBy('total', "desc")
+                ->limit(25)
+                ->get();
+        });
     }
 }
